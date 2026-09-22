79d64f270ea881680f78c3259bd3bd56d4dedf2d
Geeoon Chung <geeoon13@gmail.com>
2026-09-22 15:02:12 -0700

    Attempt to create optimized parameterized popcount

--- rtl/cross_correlator/popcount.sv
+++ rtl/cross_correlator/popcount.sv
@@ -10,72 +10,89 @@
  * @param[out] out_arr  the solution
  * @param[out] valid    whether the output is valid or not
  */
+
+// Constant function to compute ceil(log_base(value))
+function automatic integer clogb_n;
+    input integer base;
+    input integer value;
+    integer temp;
+    begin
+        clogb_n = 0;
+        temp = 1;
+        while (temp < value) begin
+            temp = temp * base;
+            clogb_n = clogb_n + 1;
+        end
+    end
+endfunction
+
 module popcount #(
-    parameter int DEPTH
-    // parameter int IN_LENGTH,
-    // parameter int LUT_SIZE=6,
-    // parameter int ADDER_SIZE=3
+    parameter int IN_LENGTH,
+    parameter int LUT_SIZE=6,
+    parameter int ADDER_SIZE=3,
+
+    localparam int DEPTH=clogb_n(ADDER_SIZE, IN_LENGTH / LUT_SIZE),
+    localparam int TOTAL_SIZE=LUT_SIZE * (ADDER_SIZE**(DEPTH-1)),
+    localparam int OUTPUT_BITS=$clog2(IN_LENGTH+1),
+    localparam int SUBSECTION_SIZE=TOTAL_SIZE/ADDER_SIZE
 )(
     input logic clk,
     input logic start,
-    input logic [(2**DEPTH)-1:0] in_arr,
+    input logic [IN_LENGTH-1:0] in_arr,
 
-    output logic [DEPTH:0] out_arr,
+    output logic [OUTPUT_BITS-1:0] out_arr,
     output logic valid
 );
-    // Constant function to compute ceil(log_base(value))
-    // function automatic integer clogb_n;
-    //     input integer base;
-    //     input integer value;
-    //     integer temp;
-    //     begin
-    //         clogb_n = 0;
-    //         temp = 1;
-    //         while (temp < value) begin
-    //             temp = temp * base;
-    //             clogb_n = clogb_n + 1;
-    //         end
-    //     end
-    // endfunction
-    
-    // localparam int DEPTH = clogb_n(ADDER_SIZE, IN_LENGTH / LUT_SIZE) + 1;
-        
+    assign padded = in_arr;  // pads to expected lengths
     generate
-        if (DEPTH == 1)
+        if (DEPTH == 1)  // base case
             begin : DEPTH_eq_1
+                // LUT compressor logic
+                logic [OUTPUT_BITS-1:0] compressor;
+                always_comb begin
+                    compressor = '0;
+                    for (genvar i = 0; i < LUT_SIZE; i++) begin
+                        compressor = compressor + {OUTPUT_BITS}'(input[i]);
+                    end  // for
+                end  // always_comb
+                // pipeline DFF
                 always_ff @(posedge clk) begin
                     valid <= start;
-                    out_arr <= in_arr[0] + in_arr[1];
+                    out_arr <= compressor;
                 end  // always_ff
             end  // DEPTH_eq_1
         else
-            begin : DEPTH_gt_1
-                logic [DEPTH-1:0] left_out, right_out;
-                logic left_valid, right_valid;
+            begin : DEPTH_gt_1  // recursive case
+                logic [($clog2(SUBSECTION_SIZE+1))-1:0] outs [0:ADDER_SIZE-1];
+                logic valids [0:ADDER_SIZE-1];
+
+                // submodules
+                for (genvar i = 0; i < ADDER_SIZE; i++) begin
+                    popcount #(
+                        .IN_LENGTH(SUBSECTION_SIZE),
+                        .LUT_SIZE(LUT_SIZE),
+                        .ADDER_SIZE(ADDER_SIZE)
+                    ) popcount_section (
+                        .clk,
+                        .start,
+                        .in_arr(in_arr[((i+1)*SUBSECTION_SIZE)-1:i*SUBSECTION_SIZE]),
+                        .out_arr(outs[i]),
+                        .valid(valids[i])
+                    );
+                end  // for
+
+                // Adder optimization
+                logic [OUTPUT_BITS-1:0] sum;
+                always_comb begin
+                    for (int i = 0; i < ADDER_SIZE; i++) begin
+                        sum = sum + {OUTPUT_SIZE}'(outs[i]);
+                    end  // for
+                end  // always_comb
                 
-                popcount #(
-                    .DEPTH(DEPTH-1)
-                ) popcount_left (
-                    .clk,
-                    .start,
-                    .in_arr(in_arr[(2**DEPTH)-1:2**(DEPTH-1)]),
-                    .out_arr(left_out),
-                    .valid(left_valid)
-                );
-
-                popcount #(
-                    .DEPTH(DEPTH-1)
-                ) popcount_right (
-                    .clk,
-                    .start,
-                    .in_arr(in_arr[(2**(DEPTH-1))-1:0]),
-                    .out_arr(right_out),
-                    .valid(right_valid)
-                );
-
+                // pipeline
                 always_ff @(posedge clk) begin
-                    valid <= left_valid & right_valid;
-                    out_arr <= left_out + right_out;  // NOTE: this addition is probably a bottleneck
+                    valid <= &valids;
+                    out_arr <= sum;
                 end  // always_ff
             end  // DEPTH_gt_1
     endgenerate
